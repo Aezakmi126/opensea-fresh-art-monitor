a28a60540df6548adc87d8f7d3ac69c6c73b8e05
Aezakmi126 <mr.sergivanov1990@icloud.com>
2026-09-12 09:57:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ def get_account_mints(address):
         params=params,
     )
 
-   print("ACCOUNT_MINTS:", address, "count =", len(data.get("asset_events", [])))   
+    print("ACCOUNT_MINTS:", address, "count =", len(data.get("asset_events", [])))
     return data.get("asset_events", [])
 
 
